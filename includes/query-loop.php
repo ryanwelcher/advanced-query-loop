59fe81a197f8d07ea580697f69c04a7f168fa6ee
Ryan Welcher <me@ryanwelcher.com>
2023-01-01 16:29:18 -0500

    Move error checking to new file location.

--- includes/query-loop.php
+++ includes/query-loop.php
@@ -19,72 +19,80 @@ namespace AdvancedQueryLoop;
 				function( $default_query ) use ( $parsed_block ) {
 					$custom_query = $parsed_block['attrs']['query'];
 					// Generate a new custom query will all potential query vars.
+					$custom_args = array();
 
-					$meta_queries = array(
-						'relation' => $custom_query['meta_query']['relation'],
-					);
+					// Check for meta queries.
+					if ( isset( $custom_query['meta_query']['queries'] ) ) {
 
-					foreach ( $custom_query['meta_query']['queries'] as $query ) {
-						$meta_queries[] = array_filter(
-							array(
-								'key'     => $query['meta_key'],
-								'value'   => $query['meta_value'],
-								'compare' => $query['meta_compare'],
-							)
+						$meta_queries = array(
+							'relation' => isset( $custom_query['meta_query']['relation'] ) ? $custom_query['meta_query']['relation'] : '',
 						);
+
+						foreach ( $custom_query['meta_query']['queries'] as $query ) {
+							$meta_queries[] = array_filter(
+								array(
+									'key'     => $query['meta_key'],
+									'value'   => $query['meta_value'],
+									'compare' => $query['meta_compare'],
+								)
+							);
+						}
+
+						// Add the meta queries to the custom args.
+						$custom_args['meta_query'] = array_filter( $meta_queries ); // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
 					}
 
-					// Date related.
-					$date_query        = $custom_query['date_query'];
-					$date_relationship = $date_query['relation'];
-					$date_is_inclusive = $date_query['inclusive'];
-					$date_primary      = $date_query['date_primary'];
-					$date_secondary    = $date_query['date_secondary'];
+					// Date queries.
+					if ( isset( $custom_query['date_query'] ) ) {
+						$date_query        = $custom_query['date_query'];
+						$date_relationship = $date_query['relation'];
+						$date_is_inclusive = $date_query['inclusive'];
+						$date_primary      = $date_query['date_primary'];
+						$date_secondary    = $date_query['date_secondary'];
 
-					// Date format: 2022-12-27T11:14:21.
-					$primary_year    = substr( $date_primary, 0, 4 );
-					$primary_month   = substr( $date_primary, 5, 2 );
-					$primary_day     = substr( $date_primary, 8, 2 );
-					$secondary_year  = substr( $date_secondary, 0, 4 );
-					$secondary_month = substr( $date_secondary, 5, 2 );
-					$secondary_day   = substr( $date_secondary, 8, 2 );
+						// Date format: 2022-12-27T11:14:21.
+						$primary_year    = substr( $date_primary, 0, 4 );
+						$primary_month   = substr( $date_primary, 5, 2 );
+						$primary_day     = substr( $date_primary, 8, 2 );
+						$secondary_year  = substr( $date_secondary, 0, 4 );
+						$secondary_month = substr( $date_secondary, 5, 2 );
+						$secondary_day   = substr( $date_secondary, 8, 2 );
 
-					if ( 'between' === $date_relationship ) {
-						$date_queries = array(
-							'after'  => array(
-								'year'  => $primary_year,
-								'month' => $primary_month,
-								'day'   => $primary_day,
-							),
-							'before' => array(
-								'year'  => $secondary_year,
-								'month' => $secondary_month,
-								'day'   => $secondary_day,
-							),
-						);
-					} else {
-						$date_queries = array(
-							$date_relationship => array(
-								'year'  => $primary_year,
-								'month' => $primary_month,
-								'day'   => $primary_day,
-							),
-						);
+						if ( 'between' === $date_relationship ) {
+							$date_queries = array(
+								'after'  => array(
+									'year'  => $primary_year,
+									'month' => $primary_month,
+									'day'   => $primary_day,
+								),
+								'before' => array(
+									'year'  => $secondary_year,
+									'month' => $secondary_month,
+									'day'   => $secondary_day,
+								),
+							);
+						} else {
+							$date_queries = array(
+								$date_relationship => array(
+									'year'  => $primary_year,
+									'month' => $primary_month,
+									'day'   => $primary_day,
+								),
+							);
+						}
+
+						$date_queries['inclusive'] = $date_is_inclusive;
+
+						// Add the date queries to the custom query.
+						$custom_args['date_query'] = array_filter( $date_queries );
+
 					}
 
-					$date_queries['inclusive'] = $date_is_inclusive;
-
-					$custom_args = array(
-						'meta_query' => array_filter( $meta_queries ),
-						'date_query' => array_filter( $date_queries ),
-					);
-
-					$new_query = array_merge(
+					// Return the merged query.
+					return array_merge(
 						$default_query,
 						$custom_args
 					);
-					// Filter out any empty values from the custom query and merge it with the existing query.
-					return $new_query;
 				},
 				10,
 				2
@@ -123,72 +131,73 @@ namespace AdvancedQueryLoop;
  */
 function add_custom_query_params( $args, $request ) {
 	// Generate a new custom query will all potential query vars.
-
+	$custom_args = array();
 	// Meta related.
-	$meta_query   = $request->get_param( 'meta_query' );
-	$meta_queries = array(
-		'relation' => $meta_query['relation'],
-	);
-	foreach ( $meta_query['queries'] as $query ) {
-		$meta_queries[] = array_filter(
-			array(
-				'key'     => $query['meta_key'],
-				'value'   => $query['meta_value'],
-				'compare' => $query['meta_compare'],
-			)
-		);
-	}
+	$meta_query = $request->get_param( 'meta_query' );
 
+	if ( $meta_query ) {
+		$meta_queries = array(
+			'relation' => $meta_query['relation'],
+		);
+		foreach ( $meta_query['queries'] as $query ) {
+			$meta_queries[] = array_filter(
+				array(
+					'key'     => $query['meta_key'],
+					'value'   => $query['meta_value'],
+					'compare' => $query['meta_compare'],
+				)
+			);
+		}
+
+		$custom_args['meta_query'] = array_filter( $meta_queries );
+	}
 	// Date related.
-	$date_query        = $request->get_param( 'date_query' );
-	$date_relationship = $date_query['relation'];
-	$date_is_inclusive = ( 'true' === $date_query['inclusive'] ) ? true : false;
-	$date_primary      = $date_query['date_primary'];
-	$date_secondary    = $date_query['date_secondary'];
+	$date_query = $request->get_param( 'date_query' );
 
-	// Date format: 2022-12-27T11:14:21.
-	$primary_year    = substr( $date_primary, 0, 4 );
-	$primary_month   = substr( $date_primary, 5, 2 );
-	$primary_day     = substr( $date_primary, 8, 2 );
-	$secondary_year  = substr( $date_secondary, 0, 4 );
-	$secondary_month = substr( $date_secondary, 5, 2 );
-	$secondary_day   = substr( $date_secondary, 8, 2 );
+	if ( $date_query ) {
+		$date_relationship = $date_query['relation'];
+		$date_is_inclusive = ( 'true' === $date_query['inclusive'] ) ? true : false;
+		$date_primary      = $date_query['date_primary'];
+		$date_secondary    = $date_query['date_secondary'];
 
-	if ( 'between' === $date_relationship ) {
-		$date_queries = array(
-			'after'  => array(
-				'year'  => $primary_year,
-				'month' => $primary_month,
-				'day'   => $primary_day,
-			),
-			'before' => array(
-				'year'  => $secondary_year,
-				'month' => $secondary_month,
-				'day'   => $secondary_day,
-			),
-		);
-	} else {
-		$date_queries = array(
-			$date_relationship => array(
-				'year'  => $primary_year,
-				'month' => $primary_month,
-				'day'   => $primary_day,
-			),
-		);
+		// Date format: 2022-12-27T11:14:21.
+		$primary_year    = substr( $date_primary, 0, 4 );
+		$primary_month   = substr( $date_primary, 5, 2 );
+		$primary_day     = substr( $date_primary, 8, 2 );
+		$secondary_year  = substr( $date_secondary, 0, 4 );
+		$secondary_month = substr( $date_secondary, 5, 2 );
+		$secondary_day   = substr( $date_secondary, 8, 2 );
+
+		if ( 'between' === $date_relationship ) {
+			$date_queries = array(
+				'after'  => array(
+					'year'  => $primary_year,
+					'month' => $primary_month,
+					'day'   => $primary_day,
+				),
+				'before' => array(
+					'year'  => $secondary_year,
+					'month' => $secondary_month,
+					'day'   => $secondary_day,
+				),
+			);
+		} else {
+			$date_queries = array(
+				$date_relationship => array(
+					'year'  => $primary_year,
+					'month' => $primary_month,
+					'day'   => $primary_day,
+				),
+			);
+		}
+		$date_queries['inclusive'] = $date_is_inclusive;
+
+		$custom_args['date_query'] = array_filter( $date_queries );
 	}
-	$date_queries['inclusive'] = $date_is_inclusive;
 
 	// Merge all queries.
-	$custom_args = array(
-		'meta_query' => array_filter( $meta_queries ),
-		'date_query' => array_filter( $date_queries ),
-	);
-
-	$new_query = array_merge(
+	return array_merge(
 		$args,
 		array_filter( $custom_args )
 	);
-
-	// Filter out any empty values from the custom query and merge it with the existing query.
-	return $new_query;
 }
