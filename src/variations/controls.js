331ecda646094a55951013ff5e5dd942b7919edd
Ryan Welcher <me@ryanwelcher.com>
2024-08-14 13:15:13 -0400

    Create and expose a new internal slot so we can re-insert the legacy controls.

--- src/variations/controls.js
+++ src/variations/controls.js
@@ -12,8 +12,7 @@ import { createBlock } from '@wordpress/blocks';
 import { AQL } from '.';
 import AQLControls from '../slots/aql-controls';
 import AQLControlsInheritedQuery from '../slots/aql-controls-inherited-query';
-import { PostCountControls } from '../components/post-count-controls';
-import { PostOffsetControls } from '../components/post-offset-controls';
+import AQLLegacyControls from '../slots/aql-legacy-controls';
 import { PostMetaQueryControls } from '../components/post-meta-query-controls';
 import { PostDateQueryControls } from '../components/post-date-query-controls';
 import { MultiplePostSelect } from '../components/multiple-post-select';
@@ -56,9 +55,10 @@ const withAdvancedQueryControls = ( BlockEdit ) => ( props ) => {
 								'advanced-query-loop'
 							) }
 						>
+							<AQLLegacyControls.Slot
+								fillProps={ { ...props } }
+							/>
 							<MultiplePostSelect { ...props } />
-							<PostCountControls { ...props } />
-							<PostOffsetControls { ...props } />
 							<PostOrderControls { ...props } />
 							<PostExcludeControls { ...props } />
 							<PostIncludeControls { ...props } />
