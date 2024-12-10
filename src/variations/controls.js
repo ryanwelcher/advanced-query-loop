6db0fd730ea2cae2208091792c45a9eea215dc96
Ryan Welcher <me@ryanwelcher.com>
2024-12-09 19:18:31 -0500

    Add more whitespace.

--- src/variations/controls.js
+++ src/variations/controls.js
@@ -6,6 +6,7 @@ import { InspectorControls } from '@wordpress/block-editor';
 import { PanelBody } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { createBlock } from '@wordpress/blocks';
+
 /**
  *  Internal dependencies
  */
