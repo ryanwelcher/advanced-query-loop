7907ac69a359238d5fe4053d30355d78b5cb2f6f
Ryan Welcher <me@ryanwelcher.com>
2025-03-13 11:48:48 -0400

    Integration tests set up and first test is passing!

--- tests/integration/tests/Filter_Tests.php
+++ tests/integration/tests/Filter_Tests.php
@@ -11,14 +11,15 @@ use AdvancedQueryLoop\Query_Params_Generator;
  */
 class AQL_Filter_Tests extends \WP_UnitTestCase {
 
-	public function test_tax_query() {
+	public function test_tax_query_add_items_to_custom_tax_query() {
 
 		$expected_query_args = [
 			'tax_query' => [
 				[
-					'taxonomy' => 'category',
-					'field'    => 'slug',
-					'terms'    => 'test',
+					'taxonomy'         => 'category',
+					'terms'            => [ 1 ],
+					'include_children' => true,
+					'operator'         => 'IN',
 				],
 				[
 					'taxonomy'         => 'recipe_category',
@@ -30,34 +31,38 @@ class AQL_Filter_Tests extends \WP_UnitTestCase {
 			],
 		];
 
-		$default_query = [
+		$default_params_from_block = [];
+
+		$custom_params_from_aql = [
 			'tax_query' => [
-				[
-					'taxonomy' => 'category',
-					'field'    => 'slug',
-					'terms'    => 'test',
+				'queries'  => [
+					[
+						'id'               => '9aa9887e-e61b-42e0-bc5d-3b8c60337a3b',
+						'taxonomy'         => 'category',
+						'terms'            => [ 'Uncategorized' ],
+						'include_children' => true,
+						'operator'         => 'IN',
+					],
 				],
 			],
 		];
 
-		$block_query = [];
-
-		$qpg = new Query_Params_Generator( $default_query, $block_query );
+		$qpg = new Query_Params_Generator( $default_params_from_block, $custom_params_from_aql );
 
 		add_filter(
 			'aql_query_vars',
-			function ( $query_args ) {
+			function ( $query_args, $default_params, $context ) {
 				$query_args['tax_query'][] = array(
-					array(
-						'taxonomy'         => 'recipe_category',
-						'field'            => 'slug',
-						'terms'            => 'pasta',
-						'operator'         => 'NOT IN',
-						'include_children' => false,
-					),
+					'taxonomy'         => 'recipe_category',
+					'field'            => 'slug',
+					'terms'            => 'pasta',
+					'operator'         => 'NOT IN',
+					'include_children' => false,
 				);
 				return $query_args;
-			}
+			},
+			10,
+			3
 		);
 
 		$qpg->process_all();
@@ -65,10 +70,13 @@ class AQL_Filter_Tests extends \WP_UnitTestCase {
 		$filtered_query_args = \apply_filters(
 			'aql_query_vars',
 			$query_args,
-			$block_query,
+			$default_params_from_block,
 			false
 		);
 		// Assertions
-		$this->assertEqualsCanonicalizing( $expected_query_args, array_merge( $default_query, $filtered_query_args ) );
+		$this->assertSame( $expected_query_args, array_merge( $default_params_from_block, $filtered_query_args ) );
 	}
+
+	public function test_tax_query_add_items_to_default_tax_query() {}
+
 }
