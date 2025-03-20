e04311b84f03d58aa94e58dba49ab922b08a849c
mehidi258 <mehidihassan258@gmail.com>
2025-03-20 14:36:41 +0600

    Fix post type merge issue to retain default post type on frontend

--- includes/Traits/Multiple_Posts.php
+++ includes/Traits/Multiple_Posts.php
@@ -13,6 +13,12 @@ trait Multiple_Posts {
 	 * Main processing function.
 	 */
 	public function process_multiple_posts(): void {
-		$this->custom_args['post_type'] = array_unique( array_merge( array( $this->default_params['post_type'] ), $this->custom_params['multiple_posts'] ), SORT_REGULAR );
+		$this->custom_args['post_type'] = array_unique(
+			array_merge(
+				(array) $this->default_params['post_type'],
+				(array) $this->custom_params['multiple_posts']
+			),
+			SORT_REGULAR
+		);
 	}
 }
