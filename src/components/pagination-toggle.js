a265254a49f9c42a6f8382c1b865bec9872bd829
Ryan Welcher <me@ryanwelcher.com>
2025-05-26 13:11:38 -0400

    Remove unneeded items from the component.

--- src/components/pagination-toggle.js
+++ src/components/pagination-toggle.js
@@ -1,22 +1,8 @@
 /**
  * WordPress dependencies
  */
-import { store as blockEditorStore } from '@wordpress/block-editor';
 import { ToggleControl } from '@wordpress/components';
-import { useSelect } from '@wordpress/data';
-import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-
-/**
- * Check for the core/query-pagination block.
- *
- * @param {Array} blocks The array of innerBlocks
- * @return {string|boolean} Return either the clientId or false if not found.
- */
-const getPaginationBlockClientId = ( blocks ) => {
-	return blocks.find( ( block ) => block.name === 'core/query-pagination' )
-		?.clientId;
-};
 
 export const PaginationToggle = ( { attributes, setAttributes } ) => {
 	const { query: { disable_pagination: disablePagination } = {} } =
