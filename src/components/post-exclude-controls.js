e99dfd10b63f56a169ab54829d33c8f962af8934
Daryll Doyle <daryll.doyle@10up.com>
2025-08-05 10:44:42 +0100

    Exclude post controls

--- src/components/post-exclude-controls.js
+++ src/components/post-exclude-controls.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { ToggleControl } from '@wordpress/components';
+import { ToggleControl, FormTokenField, BaseControl } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { useEntityRecord, store as coreDataStore } from '@wordpress/core-data';
 import { __ } from '@wordpress/i18n';
@@ -21,7 +21,49 @@ export const PostExcludeControls = ( {
 	setAttributes,
 	allowedControls,
 } ) => {
-	const { query: { exclude_current: excludeCurrent } = {} } = attributes;
+	const { query: { exclude_current: excludeCurrent, exclude_posts: excludePosts = [], } = {} } = attributes;
+
+	// If the control is not allowed, return null.
+	if ( ! allowedControls.includes( 'exclude_current_post' ) && ! allowedControls.includes( 'exclude_posts' ) ) {
+		return null;
+	}
+
+	return (
+		<>
+			<h2> { __( 'Exclude Posts', 'advanced-query-loop' ) }</h2>
+			<ExcludeCurrentPostControl
+				attributes={ attributes }
+				setAttributes={ setAttributes }
+				allowedControls={ allowedControls }
+			/>
+			<ExcludePostsControl
+				attributes={ attributes }
+				setAttributes={ setAttributes }
+				allowedControls={ allowedControls }
+			/>
+		</>
+	);
+};
+
+/**
+ * ExcludeCurrentPostControl is a React functional component used within the context
+ * of advanced query loop settings. It toggles the exclusion of the current post
+ * or content associated with the current template from query results.
+ *
+ * @param {Object} props                   The properties passed to the component.
+ * @param {Object} props.attributes        The block attributes.
+ * @param {Function} props.setAttributes   Function to update block attributes.
+ * @param {Array} props.allowedControls    List of control identifiers that are allowed for this block.
+ *
+ * @return {Element|null} A `ToggleControl` component if the control is allowed, or `null` if not.
+ */
+const ExcludeCurrentPostControl = ( { attributes, setAttributes, allowedControls } ) => {
+	const { query: { exclude_current: excludeCurrent, } = {} } = attributes;
+
+	if ( ! allowedControls.includes( 'exclude_current_post' ) ) {
+		return null;
+	}
+
 	const { record: siteOptions } = useEntityRecord( 'root', 'site' );
 	const { currentPost, isAdmin } = useSelect( ( select ) => {
 		return {
@@ -32,11 +74,6 @@ export const PostExcludeControls = ( {
 			} ),
 		};
 	}, [] );
-
-	// If the control is not allowed, return null.
-	if ( ! allowedControls.includes( 'exclude_current_post' ) ) {
-		return null;
-	}
 
 	if ( ! currentPost ) {
 		return <div>{ __( 'Loading…', 'advanced-query-loop' ) }</div>;
@@ -62,33 +99,108 @@ export const PostExcludeControls = ( {
 	};
 
 	return (
-		<>
-			<h2> { __( 'Exclude Posts', 'advanced-query-loop' ) }</h2>
-			<ToggleControl
-				__nextHasNoMarginBottom
-				label={ __( 'Exclude Current Post', 'advanced-query-loop' ) }
-				checked={ !! excludeCurrent }
-				disabled={ isDisabled() }
-				onChange={ ( value ) => {
+		<ToggleControl
+			__nextHasNoMarginBottom
+			label={ __( 'Exclude Current Post', 'advanced-query-loop' ) }
+			checked={ !! excludeCurrent }
+			disabled={ isDisabled() }
+			onChange={ ( value ) => {
+				setAttributes( {
+					query: {
+						...attributes.query,
+						exclude_current: value ? currentPost.id : 0,
+					},
+				} );
+			} }
+			help={
+				isDisabled()
+					? __(
+						'This option is disabled for this template as there is no dedicated post to exclude.',
+						'advanced-query-loop'
+					)
+					: __(
+						'Remove the associated post for this template/content from the query results.',
+						'advanced-query-loop'
+					)
+			}
+		/>
+	);
+}
+
+const ExcludePostsControl = ( { attributes, setAttributes, allowedControls } ) => {
+	const {
+		query: {
+			exclude_posts: excludePosts = [],
+			multiple_posts: multiplePosts = [],
+			postType
+		} = {}
+	} = attributes;
+
+	if ( ! allowedControls.includes( 'exclude_posts' ) ) {
+		return null;
+	}
+
+	// Get the posts for all post types used in the query.
+	const posts = useSelect(
+		( select ) => {
+			const { getEntityRecords } = select( 'core' );
+
+			// Fetch posts for each post type and combine them into one array
+			return [ ...multiplePosts, postType ].reduce(
+				( accumulator, postType ) => {
+					// Depending on the number of posts this could take a while, since we can't paginate here
+					const records = getEntityRecords( 'postType', postType, {
+						per_page: -1,
+					} );
+					return [ ...accumulator, ...( records || [] ) ];
+				},
+				[]
+			);
+		},
+		[ postType, multiplePosts ]
+	);
+
+	// For use with flatMap(), as this lets us remove elements during a map()
+	const idToTitle = ( id ) => {
+		const post = posts.find( ( p ) => p.id === id );
+		return post ? [ post.title.rendered ] : [];
+	};
+
+	const titleToId = ( title ) => {
+		const post = posts.find( ( p ) => p.title.rendered === title );
+		return post ? [ post.id ] : [];
+	};
+
+	if ( ! posts ) {
+		return <div>{ __( 'Loading…', 'advanced-query-loop' ) }</div>;
+	}
+
+	return (
+		<BaseControl
+			help={ __(
+				'Start typing to search for a post title to exclude, or manually enter one.',
+				'advanced-query-loop'
+			) }
+		>
+			<FormTokenField
+				label={ __( 'Posts to Exclude', 'advanced-query-loop' ) }
+				value={ excludePosts.flatMap( ( id ) => idToTitle( id ) ) }
+				suggestions={ posts.map( ( post ) => post.title.rendered ) }
+				onChange={ ( titles ) => {
+					// Converts the Titles to Post IDs before saving them
 					setAttributes( {
 						query: {
 							...attributes.query,
-							exclude_current: value ? currentPost.id : 0,
+							exclude_posts:
+								titles.flatMap( ( title ) =>
+									titleToId( title )
+								) || [],
 						},
 					} );
 				} }
-				help={
-					isDisabled()
-						? __(
-								'This option is disabled for this template as there is no dedicated post to exclude.',
-								'advanced-query-loop'
-						  )
-						: __(
-								'Remove the associated post for this template/content from the query results.',
-								'advanced-query-loop'
-						  )
-				}
+				__experimentalExpandOnFocus
+				__experimentalShowHowTo={ false }
 			/>
-		</>
-	);
-};
+		</BaseControl>
+	)
+}
