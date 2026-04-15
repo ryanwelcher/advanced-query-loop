ed7498596dc4b8ee50c1cbfbe33ae7bf90741da6
Robert O'Rourke <23417+roborourke@users.noreply.github.com>
2026-04-15 16:14:19 +0100

    Implement dynamic date functions for meta queries
    Added support for dynamic date functions in meta queries.

--- includes/Traits/Meta_Query.php
+++ includes/Traits/Meta_Query.php
@@ -20,6 +20,10 @@ trait Meta_Query {
 
 			if ( isset( $meta_query_data['queries'] ) ) {
 				foreach ( $meta_query_data['queries'] as $query ) {
+					if ( in_array( $query['meta_type'] ?? '', [ 'DATETIME', 'DATE', 'TIME' ], true ) ) {
+						$query['meta_value'] = $this->map_date_functions( $query['meta_value'] ?? '' );
+					}
+					
 					$meta_queries[] = array_filter(
 						array(
 							'key'     => $query['meta_key'] ?? '',
@@ -33,5 +37,36 @@ trait Meta_Query {
 		}
 
 		return array_filter( $meta_queries );
+	}
+
+	/**
+	 * Enable a selection of dynamic date functions for meta queries.
+	 *
+	 * @param string $value Argument to be passed to WP_Meta_Query.
+	 */
+	protected function map_date_functions( $value ) {
+		$value = str_ireplace(
+			[
+				'NOW()',
+				'HOUR()',
+				'DAY()',
+				'MONTH()',
+				'YEAR()',
+				'WEEK()',
+				'UNIX_TIMESTAMP()',
+			],
+			[
+				current_time( 'Y-m-d H:i:s' ),
+				intval( date( 'H' ) ),
+				intval( date( 'd' ) ),
+				intval( date( 'm' ) ),
+				intval( date( 'Y' ) ),
+				intval( date( 'W' ) ),
+				time(),
+			],
+			$value
+		);
+	
+		return $value;
 	}
 }
